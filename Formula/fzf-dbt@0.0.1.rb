b8f62f3ee2a3abe35d6dd612cad838e62e17025c
Gaëtan Pierre-Justin <gaetan.pierrejustin@vista.com>
2023-02-14 21:27:30 +0100

    fix: incorrect format

--- Formula/fzf-dbt@0.0.1.rb
+++ Formula/fzf-dbt@0.0.1.rb
@@ -1,13 +1,12 @@
 class FzfDbtAT001 < Formula
-
   desc "Fuzy finder using fzf to look for dbt models"
   homepage "https://github.com/PJGaetan/fzf-dbt"
   url "https://github.com/PJGaetan/fzf-dbt/archive/refs/tags/v0.0.1.tar.gz"
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
   license ""
 
-  depends_on "fzf"
   depends_on "bat"
+  depends_on "fzf"
   depends_on "jq"
 
   def install
