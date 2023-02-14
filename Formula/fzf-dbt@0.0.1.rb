841f8abc9e80cf039e651baa9c983e2ddbfde902
Gaëtan Pierre-Justin <gaetan.pierrejustin@vista.com>
2023-02-14 21:38:52 +0100

    fix: shasum

--- Formula/fzf-dbt@0.0.1.rb
+++ Formula/fzf-dbt@0.0.1.rb
@@ -2,7 +2,7 @@ class FzfDbtAT001 < Formula
   desc "Fuzy finder using fzf to look for dbt models"
   homepage "https://github.com/PJGaetan/fzf-dbt"
   url "https://github.com/PJGaetan/fzf-dbt/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  sha256 "fa7f8f39e84f142ce5eced3ec3d42a6cdc4db596b9c3cf3f467b2bfc7eff3ff0"
   license ""
 
   depends_on "bat"
