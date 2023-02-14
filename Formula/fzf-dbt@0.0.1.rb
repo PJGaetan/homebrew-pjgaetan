3f258794fe22a4fecbd87e75f005a7c4b51e7fb1
Gaëtan Pierre-Justin <gaetan.pierrejustin@vista.com>
2023-02-14 21:52:45 +0100

    fix: script name

--- Formula/fzf-dbt@0.0.1.rb
+++ Formula/fzf-dbt@0.0.1.rb
@@ -10,7 +10,7 @@ class FzfDbtAT001 < Formula
   depends_on "jq"
 
   def install
-    system "cp src/fzf-dbt.sh ~/.fzf-dbt/fzf-dbt.sh"
+    system "cp src/fzf_dbt.sh ~/.fzf-dbt/fzf-dbt.sh"
     system "echo 'FZF_DBT_PATH=~/.fzf-dbt/fzf-dbt.sh' >> ~/.zshrc"
   end
 
