40788303f32832bd301f3710b4e6f6ddd7279a1c
Gaëtan Pierre-Justin <gaetan.pierrejustin@vista.com>
2023-05-30 01:23:42 +0200

    feat:airflow-cli

---
+++ Formula/airflow-cli.rb
@@ -0,0 +1,26 @@
+class Glab < Formula
+  desc "Open-source Airflow REST API command-line tool"
+  homepage "https://github.com/PJGaetan/airflow-cli"
+  url "https://github.com/PJGaetan/airflow-cli/archive/refs/tags/0.0.1.tar.gz"
+  sha256 "067f6429825463b32879fa704b57c5c490330807d96cd96d633d6d65a32977b0"
+  license "MIT"
+  head "https://github.com/PJGaetan/airflow-cli.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    if build.head?
+      system "make", "install"
+      bin.install ENV["GOPATH"] + "/bin/airflow-cli"
+    else
+      bin.install File.exist?("bin/airflow-cli") ? "bin/airflow-cli" : "airflow-cli")
+    end
+  end
+
+  test do
+    system "git", "clone", "https://gitlab.com/cli-automated-testing/homebrew-testing.git"
+    cd "homebrew-testing" do
+      system "#{bin}/airflow-cli", "--version"
+    end
+  end
+end
