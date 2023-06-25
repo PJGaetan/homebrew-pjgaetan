0038e956d09efcb73fb8fdab87c5012e3f76857e
Gaëtan Pierre-Justin <gaetan.pierrejustin@vista.com>
2023-06-25 22:32:10 +0200

    update version

--- Formula/fzf-dbt.rb
+++ Formula/fzf-dbt.rb
@@ -1,8 +1,8 @@
 class FzfDbt < Formula
   desc "Fuzy finder using fzf to look for dbt models"
   homepage "https://github.com/PJGaetan/fzf-dbt"
-  url "https://github.com/PJGaetan/fzf-dbt/archive/refs/tags/0.1.0.tar.gz"
-  sha256 "19e36be30ceabb2a8bf3148c029cf75fbdb11fa6d167fe9ac5cc7abfeb67ef89"
+  url "https://github.com/PJGaetan/fzf-dbt/archive/refs/tags/0.1.1.tar.gz"
+  sha256 "64ef7411854a738da0f4d467d9cc15dc481bd96d15b967c1fa25c15ad7f016a8"
   license ""
 
   depends_on "bat"
@@ -10,13 +10,10 @@ class FzfDbt < Formula
   depends_on "jq"
 
   def install
-    bin.install "add_zsh_profile_safely.sh"
+    bin.install "add_zsh_fzf_dbt_profile_safely.sh"
     etc.install "src/fzf_dbt.sh" => "fzf_dbt.sh"
     system "echo 'FZF_DBT_PATH=~/.fzf-dbt/fzf-dbt.sh' >> ~/.zshrc"
-  end
-
-  def post_install
-    system "bash", "add_zsh_profile_safely.sh"
+    system "bash", "add_zsh_fzf_dbt_profile_safely.sh"
   end
 
   def
